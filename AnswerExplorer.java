a261869b67772a7c6dd536b0c070143b0991ef63
shin1hi <shinichi@shinihcis-mbp.lan>
2014-08-04 09:54:00 +0900

    mac update

---
+++ AnswerExplorer.java
@@ -0,0 +1,25 @@
+class AnswerExplorer{
+
+
+  abstract void explorer(){
+
+  }
+
+}
+
+
+class CO extends AnswerExplorer{
+
+}
+
+class EO extends AnswerExplorer{
+  void explorer(){
+    
+  }
+
+
+}
+
+class ES extends AnswerExplorer{
+
+}
